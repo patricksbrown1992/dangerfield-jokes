1099bbfd97e28e7886dca50499956faf2b2acb87
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-08-03 12:02:09 -0400

    stylized home page

--- frontend/components/splash/splashForm.jsx
+++ frontend/components/splash/splashForm.jsx
@@ -34,17 +34,24 @@ class splashForm extends React.Component {
                         <a href="https://patricksbrown1992.github.io/portfolio/" target="_blank">Portfolio</a>
                         <a href="https://www.linkedin.com/in/patricksbrown1992/" target="_blank">LinkedIn</a>
                         <a href="https://github.com/patricksbrown1992" target="_blank">Github</a>
+                        <h1>Dangerfield-ster</h1>
                     </div>
                     <div className = 'internal-links-div'>
                         <Link to='/login'>Sign In</Link>
-                        <Link to='/signup'>Sign Up</Link>
+                        <Link className = 'sign-up-link' to='/signup'>Sign Up</Link>
                     </div>
                     
                 </div>
                 <div className = 'splash-body'>
-                    <h1>Welcome to the Ferly crew</h1>
-                    <h3>I'm really excited about the opportunity</h3>
-                    <button onClick={this.demo}>This is a test</button>
+                    <div className = 'splash-body-left'>
+                        <h1>Welcome to the Ferly crew</h1>
+                        <h3>I'm really excited about the opportunity</h3>
+                        <button className = 'demo-button' onClick={this.demo}>Demo</button>
+                    </div>
+                    <div className='splash-body-right'>
+                        <img src={window.dangerfield} />
+                    </div>
+                    
                 </div>
                 
             
